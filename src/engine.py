6a55213ac479684b4f00b79eb224862a5cd88739
eerojantunen <jant.eero@gmail.com>
2025-03-07 02:57:50 +0200

    add move ordering / transposition table

--- src/engine.py
+++ src/engine.py
@@ -3,6 +3,8 @@ from evaluation import *
 import copy 
 import time
 
+move_dict = {}
+
 def run_engine_local(current_board, legal_moves, depth, maxing, max_time_parameter ,move_history = []):
     global initial_time
     global max_time
@@ -16,34 +18,58 @@ def run_engine_local(current_board, legal_moves, depth, maxing, max_time_paramet
         time_left = max_time - (time.time()-initial_time)
         if time_left <= 0:
             break
-        score_data = alphabetaminimax(current_board, legal_moves, depth, maxing, move_history)
+        current_hash = current_board.get_board_state_hash()
+        ordered_moves = order_moves(current_hash,legal_moves)
+        score_data = alphabetaminimax(current_board, ordered_moves, depth, maxing, move_history)
         if score_data and max_time - (time.time()-initial_time) > 0:
             best_move = score_data 
+            move_dict[current_hash] = best_move[1][0]
         else:
             break
         depth += 1
+    print("depth",depth-1)
     return best_move
+
+def order_moves(board_hash, legal_moves):
+    if board_hash in move_dict:
+        best_move = move_dict.get(board_hash)
+        ordered_moves = []
+        for move in legal_moves:
+            if move == best_move:
+                ordered_moves.append(move)
+        for move in legal_moves:
+            if move != best_move:
+                ordered_moves.append(move)
+        return ordered_moves
+    return legal_moves
     
 def alphabetaminimax(current_board, legal_moves, depth, maxing, move_history = [], alpha=-9999999999, beta=9999999999): 
     global initial_time
     global max_time
     if depth == 0 or legal_moves is None:
         return (evaluate(current_board), move_history)
+
+    current_hash = current_board.get_board_state_hash()
+    ordered_moves = order_moves(current_hash, legal_moves)
     
     if maxing == True: 
         max_score = (-9999999999, None)
-        for move in legal_moves:
+        for move in ordered_moves:
             if time.time()-initial_time >= max_time:
                 break
+
             new_board = copy.deepcopy(current_board) 
             new_board.make_move_board(move[0],move[1],move[2])
             new_moves = all_moves(new_board,"B")
             new_move_history = move_history.copy()
             new_move_history.append(move)
+
             score = alphabetaminimax(new_board, new_moves, depth-1, False, new_move_history, alpha, beta)     
+            
             if score:
                 if max_score[0] < score[0]:
                     max_score = score
+                    move_dict[current_hash] = move
                 alpha = max(alpha, max_score[0])
                 if beta <= alpha:
                     break
@@ -51,18 +77,23 @@ def alphabetaminimax(current_board, legal_moves, depth, maxing, move_history = [
 
     else:
         min_score = (9999999999, None)
-        for move in legal_moves:
+        
+        for move in ordered_moves:
             if time.time()-initial_time >= max_time:
                 break
+
             new_board = copy.deepcopy(current_board)
             new_board.make_move_board(move[0],move[1],move[2])
             new_moves = all_moves(new_board,"W")
             new_move_history = move_history.copy()
             new_move_history.append(move)
+            
             score = alphabetaminimax(new_board, new_moves, depth-1, True, new_move_history, alpha, beta)
             if score:
                 if min_score[0] > score[0]:
                     min_score = score
+                    move_dict[current_hash] = move
+
                 beta = min(beta, min_score[0])
                 if beta <= alpha:
                     break
--- src/game.py
+++ src/game.py
@@ -5,13 +5,6 @@ class Game:
         self.main_board = Board()
         self.max_move_time = 20
 
-    def print_piece_names(self):
-        print("White pawn: W\nWhite knight: WN\nWhite bishop: WB\nWhite rook: WR")
-        print("White queen: WQ\nWhite king: WK\nBlack pawn: B\nBlack knight: BN\nBlack bishop: BB")
-        print("Black rook: BR\nBlack queen: BQ\nBlack king: BK\n")
-
-    
-
     def run_game(self): #add mate
         self.main_board.final_print_board()
         while True:
@@ -40,9 +33,6 @@ class Game:
                     continue
             self.main_board.make_move_board(alg_notation_to_index[from_square_notation],alg_notation_to_index[to_square_notation],piece)
 
-            x = self.main_board.get_board_state_hash()
-            print(x)
-
             if len(self.main_board.piece_board["WK"]) == 0:
                 print("You win!")
                 break
@@ -52,15 +42,26 @@ class Game:
         self.main_board.setup_standard_board()
         self.run_game()
     
-    
+
 
     def load_ready_position(self):
+
         self.main_board.add_material("BKh8")
         self.main_board.add_material("BQh6")
         self.main_board.add_material("BRe6")
         self.main_board.add_material("WKg1")
         self.main_board.add_material("WQe4")
         self.main_board.add_material("WRc2")
+
+        self.main_board.add_material("BNb5")
+        self.main_board.add_material("BNe7")
+        self.main_board.add_material("Bf5")
+        self.main_board.add_material("Bg7")
+        self.main_board.add_material("WBb1")
+        self.main_board.add_material("WNb7")
+        self.main_board.add_material("Wg2")
+        self.main_board.add_material("Wh3")
+
         self.run_game()
 
     def instructions(self):
@@ -71,11 +72,16 @@ class Game:
         print("\nThe piece types are:")
         self.print_piece_names()
 
+    def print_piece_names(self):
+        print("White pawn: W\nWhite knight: WN\nWhite bishop: WB\nWhite rook: WR")
+        print("White queen: WQ\nWhite king: WK\nBlack pawn: B\nBlack knight: BN\nBlack bishop: BB")
+        print("Black rook: BR\nBlack queen: BQ\nBlack king: BK")
+
     def change_parameters(self):
         while True:
-            selection = int(input(f"1: Change amount of time computer has per move. Current: {self.max_move_time}s\n0: Return\nInput: "))
+            selection = int(input(f"\n1: Change amount of time computer has per move. Current: {self.max_move_time}s\n0: Return\nInput: "))
             if selection == 1:
-                new_max_time = int(input("New amount of time computer has per move: "))
+                new_max_time = int(input("\nNew amount of time computer has per move: "))
                 self.max_move_time = new_max_time
                 print("Updated succesfully!")
             elif selection == 0:
@@ -87,7 +93,7 @@ class Game:
     def game_selection(self):
         """ select game type (new, loaded position)"""
         while True:
-            selection = int(input("1. Setup standard board \n2. Load position from memory\n3. Instructions \n4. Change engine parameters \nInput: "))
+            selection = int(input("\n1. Setup standard board \n2. Load position from memory\n3. Instructions \n4. Change engine parameters \nInput: "))
             if selection == 1:
                 self.new_game()
                 break
